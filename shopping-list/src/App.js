9653d77a4a213541596f61c9f4a6f4e400aef81c
杨瑞奇 <grandruiqi@hotmail.com>
2018-07-25 00:48:55 +0800

    add shopping-list

---
+++ shopping-list/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Nav from './Nav';
+import './App.css';
+
+class App extends React.Component {
+	renderContent() {
+		return (
+			<span>Empty</span>
+		);
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Nav/>
+				<main className="App-content">
+				{this.renderContent()}
+				</main>
+			</div>
+		);
+	}
+}
+
+export default App;
