17a7cebedbe36953caa02ed14d610b4fda8ab155
杨瑞奇 <grandruiqi@hotmail.com>
2018-07-27 20:11:08 +0800

    add item.js

--- github-file-list/src/index.js
+++ github-file-list/src/index.js
@@ -2,20 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import PropTypes from 'prop-types'
+import Time from './time';
 import registerServiceWorker from './registerServiceWorker';
 
 const FileList = ({ files }) => (
     <table className="file-list">
         <tbody>
         {files.map(file => (
-            <React.Fragment>
-                <FileListItem key={file.id} file={file} />
-            </React.Fragment>
+            <FileListItem key={file.id} file={file} />
         ))}
         </tbody>
     </table>
 );
-
 FileList.propTypes = {
     files: PropTypes.array
 };
@@ -23,25 +21,13 @@ FileList.propTypes = {
 const FileListItem = ({ file }) => (
     <tr className="file-list-item">
         <FileName file={file}/>
+        <CommitMessage commit={file.latestCommit} />
+        <td className='age'>
+            <Time time={file.updated_at}/>
+        </td>
     </tr>
 );
 FileListItem.propTypes = {
-    file: PropTypes.object.isRequired
-};
-
-const FileIcon = ({ file }) => {
-    let icon = 'fa-file-text-o';
-    if(file.type === 'folder') {
-        icon = 'fa-folder';
-    }
-    return(
-      <td className="file-icon">
-        <i className={`fa ${icon}`}/>
-      </td>
-    );
-};
-
-FileIcon.propTypes = {
     file: PropTypes.object.isRequired
 };
 
@@ -52,10 +38,33 @@ const FileName = ({ file }) => {
             <td className="file-name">{file.name}</td>
         </React.Fragment>
     );
-}
-
+};
 FileName.propTypes = {
     file: PropTypes.object.isRequired
+};
+
+const FileIcon = ({ file }) => {
+    let icon = 'fa-file-text-o';
+    if(file.type === 'folder') {
+        icon = 'fa-folder';
+    }
+    return(
+      <td className="file-icon">
+        <i className={`fa ${icon}`} />
+      </td>
+    );
+};
+FileIcon.propTypes = {
+    file: PropTypes.object.isRequired
+};
+
+const CommitMessage = ({ commit }) => {
+    return(
+        <td className="commit-message">{commit.message}</td>
+    );
+};
+CommitMessage.propTypes = {
+    commit: PropTypes.object.isRequired
 };
 
 const testFiles = [
@@ -63,7 +72,7 @@ const testFiles = [
         id: 1,
         name: 'src',
         type: 'folder',
-        updated_at: "2016-07-11 21:24:00",
+        updated_at: "2018-07-3 21:24:00",
         latestCommit: {
         message: 'Initial commit'
         }
@@ -73,7 +82,7 @@ const testFiles = [
         id: 2,
         name: 'tests',
         type: 'folder',
-        updated_at: "2016-07-11 21:24:00",
+        updated_at: "2018-07-4 21:24:00",
         latestCommit: {
         message: 'Initial commit'
         }
@@ -83,9 +92,9 @@ const testFiles = [
         id: 3,
         name: 'README',
         type: 'file',
-        updated_at: "2016-07-18 21:24:00",
+        updated_at: "2018-07-3 21:24:00",
         latestCommit: {
-        message: 'Added a readme'
+        message: 'Add a readme'
         }
     }
 ];
--- proptypes-tweet/src/index.js
+++ proptypes-tweet/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import moment from 'moment';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import './index.css';
 
 const testTweet = {
